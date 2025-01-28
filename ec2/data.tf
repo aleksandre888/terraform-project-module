aff2a034d162795e9764ed48efb67473baec4e9c
aleksandre888 <aleksandremikashavidze@gmail.com>
2025-01-28 19:22:25 +0400

    Update data.tf
    attempted fix for terraform file path error

--- ec2/data.tf
+++ ec2/data.tf
@@ -1,5 +1,5 @@
 data "template_file" "user_data" {
-  template = file("script.sh")
+  template = file("${path.module}/script.sh")
 
   vars = {
     PACKAGE_MANAGER = var.linux_distro == "ubuntu" ? "apt" : "yum"
